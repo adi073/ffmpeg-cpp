caa131d6ad530792367722dae075e5cf30ae1972
Aditya <bhushan.adit@gmail.com>
2023-12-28 16:28:06 +0100

    feat: extract video file using libav
    Signed-off-by: Aditya <bhushan.adit@gmail.com>

--- src/load_frame.cpp
+++ src/load_frame.cpp
@@ -9,19 +9,47 @@ extern "C" {
 
 bool load_frame(const char* filename, int* width, int* height, unsigned char** data)
 {
+    // Create av format context
     AVFormatContext* av_format_ctx = avformat_alloc_context();
     if (!av_format_ctx)
     {
         std::cout << "Couldn't create AVFormatContext\n" << std::endl;
         return false;
     }
-
+    
+    //open the input and put the information about the open file into the av format context.
+    // It will return the integer/bool if it succeded or not.
     if (avformat_open_input(&av_format_ctx, filename, NULL, NULL) != 0)
     {
         std::cout << "Couldn't open video file\n" << std::endl;
         return false;
     }
+    
+    int video_stream_idx = -1;
+    AVCodecParameters *av_codec_params;
+    AVCodec *av_codec;
+    for (int i=0; i<av_format_ctx->nb_streams; ++i){
+        auto stream = av_format_ctx->nb_streams[i];
+        av_codec_params = av_format_ctx->streams[i]->codecpar;
+        av_codec = av_codec_find_decoder(av_codec_params->code_id);
+
+        if (!av_codec){
+            continue;
+        }
+
+        if (av_codec->code_type == AVMEDIA_TYPE_VIDEO){
+            video_stream_idx = i;
+            break;
+        }
+
+    }
+
+    if (video_stream_idx == -1){
+        std::cout << "Couldn't find valid video stream" << std::endl;
+        return false;
+    }
+
+
     return false;
 }
 
-1:46:00
